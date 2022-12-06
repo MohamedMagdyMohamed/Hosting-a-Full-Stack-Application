be698a110728e56941b4ad96ae18252b93097e93
MohamedMagdyMohamed <mm.medany96@gmail.com>
2022-12-06 08:05:51 +0200

    Update backend deploy script add set envirnoment

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,3 +1,4 @@
 eb init Udagram-Magdy --platform node.js --region us-east-1 --profile=default
 eb use Udagrammagdy-env-1
 eb deploy
+eb setenv JWT_SECRET=$JWT_SECRET POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME D=$D
